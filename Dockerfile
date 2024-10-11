ffbfd4f31704d099eb444b0c8eecc158e6814371
buemura <bruno.uemura1@gmail.com>
2024-10-10 22:28:11 -0300

    feat: initial project setup

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:21-alpine AS base
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+COPY .env ./
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 8080
+
+CMD ["npm", "run", "start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AllExceptionsFilter } from '@/infra/filters';
+import { LoggerInterceptor } from '@/infra/logger';
+import { AppModule } from '@/modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Preference Center API')
+    .setDescription('Preference Center API')
+    .setVersion('1.0')
+    .addTag('preference-center-api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  const port = configService.getOrThrow<number>('PORT');
+  await app.listen(port);
 }
 bootstrap();
